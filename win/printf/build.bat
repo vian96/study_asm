74a219d698c1b6e7da1bae49cdcfe303140ec885
vian96 <vianmart@yandex.ru>
2022-03-31 22:46:09 +0300

    Fixed build.bat

--- win/printf/build.bat
+++ win/printf/build.bat
@@ -2,7 +2,4 @@ nasm -f win32 printf.asm -o printf.obj -l printf.lst
 
 GoLink.exe  /dll /console printf.obj kernel32.dll  
 
-GoLink.exe  /console /entry main printf.obj kernel32.dll call.o 
-
-gcc -m32 call.c -c -S -masm=intel
-
+gcc -m32 call.c printf.dll -o call
