778ceacb9a380812b442ec458b7e13d4d07c8bd9
vian96 <vianmart@yandex.ru>
2022-05-23 06:26:48 +0300

    FIXED BUG

--- win/simd/blend.cpp
+++ win/simd/blend.cpp
@@ -9,15 +9,15 @@
 const char I = 255u, Z = 0x80u;
            
 const __m128i   _0 =                    _mm_set_epi8 (0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0);
-const __m128i _255 = _mm_cvtepi8_epi16 (_mm_set_epi8 (I,I,I,I, I,I,I,I, I,I,I,I, I,I,I,I));
+const __m128i _255 = _mm_cvtepu8_epi16 (_mm_set_epi8 (I,I,I,I, I,I,I,I, I,I,I,I, I,I,I,I));
 
-const char *FRONT_NAME = "AskhatCat.bmp";
-const char *BACK_NAME = "Table.bmp";
+const char *FRONT_NAME = "racket.bmp";
+const char *BACK_NAME = "table.bmp";
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
 
-const int LOOP_CNT = 1000;
+const int LOOP_CNT = 100;
 
 typedef RGBQUAD (&scr_t) [HEIGHT][WIDTH];
 
@@ -69,6 +69,19 @@ void blend_images (scr_t front, scr_t back, scr_t scr) {
         __m128i fr = _mm_lddqu_si128 ((__m128i*) &front[y][x]);                
         __m128i bk = _mm_lddqu_si128 ((__m128i*) &back [y][x]);
 
+        //__m128i mov = _mm_set_epi8 (12,13,14,15, 8,9,10,11, 4,5,6,7, 0,1,2,3);
+        //__m128i mov = _mm_set_epi8 (14,15,13,12, 10,11,9,8, 6,7,5,4, 2,3,1,0);
+
+        //fr = _mm_shuffle_epi8 (fr, mov);
+        //bk = _mm_shuffle_epi8 (bk, mov);
+
+
+       /* __m128i fr = _mm_set_epi32 (*(int*)&front[y][x], *(int*)&front[y][x+1], 
+                                    *(int*)&front[y][x+2], *(int*)&front[y][x+3]);
+
+        __m128i bk = _mm_set_epi32 (*(int*)&back[y][x], *(int*)&back[y][x+1], 
+                                    *(int*)&back[y][x+2], *(int*)&back[y][x+3]);
+*/
         // choose only high bytes since we need free places
         __m128i FR = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) fr);       
         __m128i BK = (__m128i) _mm_movehl_ps ((__m128) _0, (__m128) bk);
@@ -91,8 +104,8 @@ void blend_images (scr_t front, scr_t back, scr_t scr) {
         FR = _mm_mullo_epi16 (FR, A);
 
         // calculate background picture
-        bk = _mm_mullo_epi16 (bk, _mm_sub_epi8 (_255, a));                          
-        BK = _mm_mullo_epi16 (BK, _mm_sub_epi8 (_255, A));
+        bk = _mm_mullo_epi16 (bk, _mm_sub_epi16 (_255, a));                          
+        BK = _mm_mullo_epi16 (BK, _mm_sub_epi16 (_255, A));
 
         // add them up
         __m128i sum = _mm_add_epi16 (fr, bk);                                       
@@ -107,7 +120,7 @@ void blend_images (scr_t front, scr_t back, scr_t scr) {
         // combine two pixels
         __m128i color = (__m128i) _mm_movelh_ps ((__m128) sum, (__m128) SUM);  
 
-        _mm_storeu_si128 ((__m128i*) &scr[y][x], color);
+         _mm_storeu_si128 ((__m128i*) &scr[y][x], color);
     }
 }
 
